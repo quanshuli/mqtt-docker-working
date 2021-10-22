dc5c32e23dfc36cb5d9c4ebfa01503c0d8d58742
quanshuli <quanshuli.dev@gmail.com>
2021-10-22 22:00:32 +1000

    Update websocks-send-number.py

--- paho-python/mqtt-docker/websocks-send-number.py
+++ paho-python/mqtt-docker/websocks-send-number.py
@@ -1,12 +1,12 @@
 '''
-   using web socket to send numbers
+   - using paho.mqtt to send numbers through mosquitt.
+   - using websocket setting.
 '''
 import paho.mqtt.client as mqtt
 import random
 import time
 from datetime import datetime, timezone
 import json 
-import sys
 
 # websocket setting:
 broker="192.168.0.11"
@@ -17,23 +17,36 @@ port= 9001
 #port=1883
 #port= 80
 
+# define the topic 
 sub_topic="test"
 
-client= mqtt.Client("publish-socks",transport='websockets')       #create client object
+#create client object
+client= mqtt.Client("publish-socks",transport='websockets')       
 #client= paho.Client("publish-socks") # no websocket
 
-client.connect(broker,port)           #establish connection
+#establish connection
+client.connect(broker,port)           
 
+# continuously sending signals 
 while True:
+   # generate the random integer for sending
    rand_int = random.randint(1, 100)
-   #now = datetime.utcnow().isoformat() # utc time
+   
+   # get the time when sending
    now = datetime.utcnow().microsecond # utc time with microsecond only
    #now = datetime.now(timezone.utc).isoformat() # aware time object
+   #now = datetime.utcnow().isoformat() # utc time
 
+   # serializing the message into json format
    msg = json.dumps({'UTC_TIME_MICROSEC': now,
                      'NUMBER': rand_int})
-   client.publish(sub_topic,msg)    #publish
+
+   # send out the message
+   client.publish(sub_topic,msg)    
+
    print('publishing: ', msg)
+
+   # set intervals
    time.sleep(1)
 
 
